45dbe027c3aff9420f8bf08ce62ac77cd9702ccf
miguel <badayosbiboy@gmail.com>
2025-03-21 12:55:37 +0930

    added home page

---
+++ src/views/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Todo App</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1>Todo App</h1>
+    <p><em>learning php!</em></p>
+    <?php
+    require 'add_form.php';
+    ?>
+</body>
+
+</html>
