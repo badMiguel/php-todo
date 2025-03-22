f595ebaa6357fc863fe653d0364eb8a536c66126
MiguelBad <badayosbiboy@gmail.com>
2025-03-22 14:04:32 +0930

    added guide to add user and database on mysql

--- src/models/db.php
+++ src/models/db.php
@@ -2,8 +2,29 @@
 
 error_log(message: "initialising db");
 
+
 try {
-    $db = new PDO("mysql:host=localhost;dbname=todo_db", "todo_db", "password");
+
+    /*
+    it is recommended to create a new user in mysql per project. 
+    run the following command:
+        -- create the user
+        mysql -u root -p -e "CREATE USER 'todo_user'@'localhost' IDENTIFIED BY 'password'"
+        
+        -- create the database
+        mysql -u root -p -e "CREATE DATABASE todo_db"
+        
+        -- add privileges to the created user to access
+        mysql -u root -p -e "GRANT ALL PRIVILEGES ON todo_db.* TO 'todo_user'@'localhost';"
+        
+        -- apply privileges
+        mysql -u root -p -e "FLUSH PRIVILEGES"
+    */
+
+    $username = "todo_user";
+    $password = "password";
+
+    $db = new PDO("mysql:host=localhost;dbname=todo_db", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $db->exec("
